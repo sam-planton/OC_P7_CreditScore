8bf64aed202c9b8043ce0c42a52e53ee733daa59
sam-planton <samuel.planton@gmail.com>
2023-03-17 09:40:08 +0100

    Added test API script

---
+++ test_api.py
@@ -0,0 +1,30 @@
+import requests
+import pandas as pd
+
+def test_api_prediction():
+    # Load the data sample from the CSV file
+    data_sample = pd.read_csv("data_sample.csv")
+
+    # Define the API endpoint
+    endpoint = "http://127.0.0.1:5000/predict"
+
+    # Send a POST request to the API endpoint with the data as the request body
+    response = requests.post(endpoint, json=data_sample.to_dict())
+
+    # Check that the response status code is 200 OK
+    assert response.status_code == 200
+
+    # Check that the response body contains valid JSON data
+    assert response.headers["content-type"] == "application/json"
+
+    # Parse the JSON data from the response body
+    json_response = response.json()
+
+    # Check that the JSON response contains the expected keys
+    expected_keys = ["predictions"]
+    assert set(expected_keys).issubset(json_response.keys())
+
+    # Check that the predictions are in the expected format
+    predictions = json_response["predictions"]
+    assert isinstance(predictions, list)
+    assert all(isinstance(prediction, float) for prediction in predictions)
